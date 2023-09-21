ff4c721b0bba382a6ecfec2ae8cbd52bb73b6ef5
Marcio Curvello <marciocurvello@gmail.com>
2023-09-21 12:43:09 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Marcio Curvello on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
