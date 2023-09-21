cef60616da188285bbfda94414893c2c3760b8cc
Marcio Curvello <marciocurvello@gmail.com>
2023-09-21 17:52:41 -0300

    Creating books with Core Data

---
+++ Bookworm/DataController.swift
@@ -0,0 +1,13 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Marcio Curvello on 21/09/23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+}
